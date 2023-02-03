af5125bcb5b13d3372db6a885cdebe2b7bb782a7
binhk1004 <binhk1004@naver.com>
2023-02-03 23:27:17 +0900

    Jade

--- app.js
+++ app.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'jade');
+//jade 템플릿을 사용하기 위해 연결
+app.set('views', './views');
+//jade 템플릿을 모아두는 폴더와 연결
+app.locals.pretty = true;
+//소스 미리보기 시 코드 이쁘게 정렬
+
 app.use(express.static('public'));
+
+app.get('/templates', (req, res) => {
+  res.render('temp', {time: Date(),title: 'hello'});
+  //위에서 jade와 템플릿을 모아드는 폴더를 연결했으므로,
+  //렌더링할 페이지를 명명해줌(컨트롤러와 굉장히 유사)
+  //변수의 값 혹은 객체를 전달할때는 {변수명 혹은 객체명 : } 으로 사용한다.
+  //그리고 jade 템플릿에선 변수를 사용하면 된다.
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
